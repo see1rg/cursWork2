8807a422bfad140706e48b2419216e03ec898484
see1rg <see1rg@mail.ru>
2022-12-06 19:57:47 +0300

    initial commit

---
+++ src/main/java/com/skypro/curswork2/CursWork2Application.java
@@ -0,0 +1,13 @@
+package com.skypro.curswork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursWork2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursWork2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/curswork2/CursWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.curswork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
